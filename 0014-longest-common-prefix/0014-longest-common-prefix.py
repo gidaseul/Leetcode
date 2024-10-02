274b0f7ca0a7cf20e478e75916749dd83129f86a
gidaseul <61573303+gidaseul@users.noreply.github.com>
2024-10-03 01:07:19 +0900

    Time: 32 ms (87.04%), Space: 16.6 MB (74.66%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pref = strs[0]
+        pref_len = len(pref)
+
+        for i in strs[1:]:
+            while pref != i[0:pref_len]:
+                pref_len -= 1
+                if pref_len ==0:
+                    return ""
+
+                pref = pref[0:pref_len]
+
+        return pref
